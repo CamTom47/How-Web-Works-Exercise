bb97a09cf14b70e254f94b6eaf793e9a9913cd97
Cameron Thomas <camthomas47@gmail.com>
2023-12-13 07:43:25 -0500

    JS added to complete questions

---
+++ app.js
@@ -0,0 +1,22 @@
+//Part 1 
+
+/*
+- What is HTTP?
+    HTTP stands for Hyper Text Transfer Protocol and it is essentially a set of rules that are followed to ensure that requests and responses work properly.
+- What is a URL?
+    URL stands for Universal Resource Location, and it is an argument that is evaluated as a request.
+- What is DNS?
+    DNS is a Domain Name System, and it is the process in which a computer is a able to be identified and identify other computers in a network for communication.
+- What is a query string?
+    A query string is an additional arugment that can be found in a URL, prompting additional function. 
+- What are two HTTP verbs and how are they different?
+    There is HTTP and HTTPS, HTTPS is the more secure version of the two
+- What is an HTTP request?
+    This is when headers are sent to a server requesting specific information to be returned.
+- What is an HTTP response?
+    An HTTP response is the server's response, providing back headers, to the intial HTTP request. It comes back as HTML.
+- What is an HTTP header? Give a couple examples of request and response headers you have seen.
+    A HTTP header is a parameter of information. I can range from default languages, default colors, text sizes, etc. These set the standards that are passed back to the request.
+- What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+    A HTTP request is sent to a DNS. That DNS identifies the correct IP address based on the information given. I will then send the request to the correct server. Based on authorization, the server will respond with the appropriate information in the form of heads and the send the response back, which is route to the intial request location.
+*/
